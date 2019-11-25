2b2e6a92bc9d83e025fdda6026d51cc8c059e1e7
Андрей Полищук <ipolishchuk94@gmail.com>
2019-11-25 02:01:28 +0200

    Updates

---
+++ precache-manifest.c800245ba8315294841a696987ab117c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8821299104df5e118e45c35e78f2eb",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "325a07c46bf3809f6a19",
+    "url": "/test-app/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "3c15e92b32f8a05ce114",
+    "url": "/test-app/static/css/main.327756bf.chunk.css"
+  },
+  {
+    "revision": "325a07c46bf3809f6a19",
+    "url": "/test-app/static/js/2.d7911ffa.chunk.js"
+  },
+  {
+    "revision": "3c15e92b32f8a05ce114",
+    "url": "/test-app/static/js/main.739702be.chunk.js"
+  },
+  {
+    "revision": "668f06fadbc6e6e1d687",
+    "url": "/test-app/static/js/runtime-main.da81bba3.js"
+  }
+]);
